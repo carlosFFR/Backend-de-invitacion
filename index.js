e06ed2c153773d5d254a1aa1a8c9909053af3dfe
carlos <munoscesar11@gmail.com>
2024-12-11 21:26:58 -0400

    segundo commt

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
 import express  from 'express';
+import pg from 'pg';
+import { config } from 'dotenv';
+
+config()
+
 
 const app = express();
+const pool = new pg.Pool({
+ connectionString: process.env.DATABASE_URL,
+//  ssl:true
+})
+
 const port = 3000;
 
-// Ruta para manejar la solicitud POST
-app.post('/invitados', (req, res) => {
-  const { nombre, email, confirmacion, mensaje = '' } = req.body;
-
-  // Validar los datos requeridos
-  if (!nombre || !email || !confirmacion) {
-    return res.status(400).json({ error: 'Faltan datos requeridos.' });
-  }
-
-  // Crear la consulta SQL
-  const sql = 'INSERT INTO invitados (nombre, email, confirmacion, mensaje) VALUES (?, ?, ?, ?)';
-  const values = [nombre, email, confirmacion, mensaje];
-
-  // Ejecutar la consulta
-  db.query(sql, values, (err, result) => {
-    if (err) {
-      console.error('Error al insertar datos en la base de datos:', err);
-      return res.status(500).json({ error: 'Error al guardar la confirmación.' });
-    }
-
-    res.status(200).json({ message: 'Confirmación enviada correctamente.' });
-  });
-});
+app.get('/invitados',async(req,res)=>{
+  const result = await pool.query('SELECT NOW()')
+  return res.json(result.rows[0])
+})
 
 // Iniciar el servidor
 app.listen(port, () => {
